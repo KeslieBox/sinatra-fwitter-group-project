526ead1b511643e31c3ebecee8c969dfd25baa80
KeslieBox <kesliebox@gmail.com>
2021-03-02 15:37:41 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
   end
+
+   def current_user
+        User.find_by(password_digest: session[:user_id])
+    end
+
+    def logged_in?
+        # puts session.to_json
+        session.key?(:user_id)
+    end
 
 end
